a899af3cad7f8497a6afb334ceeec113b8afab25
Dennis Sergeev <dennis.sergeev@gmail.com>
2019-10-25 12:17:17 +0100

    rename aeolus/plots to aeolus/plot

---
+++ aeolus/plot/__init__.py
@@ -0,0 +1,13 @@
+"""Contains functionality for making special plots."""
+from .cart import GeoAxesGrid, label_global_map_gridlines
+from .cloud_plot_tools import CloudPlotter
+from .cm_custom import cloudtypes_cmap
+from .mpl import MidpointNormalize
+
+__all__ = [
+    CloudPlotter,
+    cloudtypes_cmap,
+    GeoAxesGrid,
+    label_global_map_gridlines,
+    MidpointNormalize,
+]
