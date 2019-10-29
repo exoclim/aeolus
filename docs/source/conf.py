76d9dbc2094b684c781237ff85117e1fef9e9341
Dennis Sergeev <dennis.sergeev@gmail.com>
2019-10-29 11:34:17 +0000

    BLD: try to fix readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -45,7 +45,6 @@ release = version
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    "sphinx.ext.autodoc",
     "sphinx.ext.extlinks",
     "sphinx.ext.intersphinx",
     "sphinx.ext.mathjax",
