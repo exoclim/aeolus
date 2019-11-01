bb39158942987a3e12de75a23865677fcaf40bdb
Dennis Sergeev <dennis.sergeev@gmail.com>
2019-11-01 16:23:22 +0000

    update requirements

--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ setup(
     package_dir={"aeolus": "aeolus"},
     packages=find_packages(),
     zip_safe=False,
+    python_requires=">=3.7",
     setup_requires=["numpy>=1.7"],
     install_requires=[
         "numpy>=1.7",
