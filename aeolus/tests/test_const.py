6c9aa0480cb1a83c266fe0d1b5e2c3c722affcac
Dennis Sergeev <dennis.sergeev@gmail.com>
2019-11-02 23:00:27 +0000

    TST: add tests for const submodule

---
+++ aeolus/tests/test_const.py
@@ -0,0 +1,71 @@
+"""Test calc submodule."""
+from pathlib import Path
+
+from aeolus import const
+from aeolus.exceptions import ArgumentError, LoadError
+
+import iris
+
+import numpy.testing as npt
+
+import pytest
+
+
+THIS_DIR = Path(__file__).parent
+CONST_FILE = "dummy"
+
+
+def test_init_const_general():
+    """Test init_const function w/o arguments."""
+    cnsts = const.init_const()
+    assert str(cnsts).startswith("GeneralConstants")
+    assert isinstance(cnsts, const.const.ConstContainer)
+    for key in cnsts.__dataclass_fields__.keys():
+        attr = getattr(cnsts, key)
+        assert isinstance(attr, const.const.ScalarCube)
+        assert attr.ndim == 0  # XXX could be relaxed
+    key = "stefan_boltzmann"
+    assert key in cnsts.__dataclass_fields__
+    cube = getattr(cnsts, key)
+    assert cube.units == "W m-2 K-4"
+    npt.assert_allclose(cube.data, 5.670367e-08)
+
+
+def test_loaderror():
+    """Test raising LoadError."""
+    with pytest.raises(LoadError):
+        const.init_const(CONST_FILE)
+    with pytest.raises(LoadError):
+        const.init_const(CONST_FILE, directory=THIS_DIR / "nonexistent_directory")
+
+
+def test_argumenterror():
+    """Test raising ArgumentError."""
+    with pytest.raises(ArgumentError):
+        const.init_const(CONST_FILE, directory=str(THIS_DIR))
+
+
+def test_init_const_custom():
+    """Test init_const function with a custom JSON file."""
+    cnsts = const.init_const(CONST_FILE, directory=THIS_DIR)
+    assert str(cnsts).startswith("DummyConstants")
+    assert isinstance(cnsts, const.const.ConstContainer)
+    for key in cnsts.__dataclass_fields__.keys():
+        attr = getattr(cnsts, key)
+        assert isinstance(attr, iris.cube.Cube)
+        assert attr.ndim == 0  # XXX could be relaxed
+    key = "my_constant"
+    assert key in cnsts.__dataclass_fields__
+    cube = getattr(cnsts, key)
+    assert cube.units == "m s-1"
+    npt.assert_allclose(cube.data, 123)
+
+
+def test_scalarcube():
+    """Test ScalarCube."""
+    name = "physical_constant"
+    cube = iris.cube.Cube(data=-123.456, units="m", long_name=name)
+    scube = const.const.ScalarCube.from_cube(cube)
+    assert isinstance(scube.asc, iris.coords.AuxCoord)
+    npt.assert_allclose(scube.data, scube.asc.points.squeeze())
+    assert scube.asc.long_name == name
