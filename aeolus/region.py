3dabf59583509178b91c3e57386fab1e9928950c
Dennis Sergeev <dennis.sergeev@gmail.com>
2019-11-21 12:21:50 +0000

    ENH: improve Region.from_cube(), replace explicit lon/lat names

--- aeolus/region.py
+++ aeolus/region.py
@@ -2,7 +2,9 @@
 from dataclasses import dataclass, field
 
 import iris
+from iris.analysis.cartography import wrap_lons
 
+from .coord_utils import UM_LATLON
 from .exceptions import BoundaryError
 from .util import fmt_lonlat
 
@@ -14,10 +16,10 @@ __all__ = ("Region",)
 class BoundsRect:
     """Bounding longitudes and latitudes of a given lon-lat rectangle."""
 
-    west: float = field(metadata={"coord": "longitude"})
-    east: float = field(metadata={"coord": "longitude"})
-    south: float = field(metadata={"coord": "latitude"})
-    north: float = field(metadata={"coord": "latitude"})
+    west: float = field(metadata={"coord": UM_LATLON[1]})
+    east: float = field(metadata={"coord": UM_LATLON[1]})
+    south: float = field(metadata={"coord": UM_LATLON[0]})
+    north: float = field(metadata={"coord": UM_LATLON[0]})
 
     def __post_init__(self):  # noqa
         # if self.west > self.east:
@@ -91,10 +93,10 @@ class Region(object):
     def _perpendicular_side_limits(self, side):
         """Get minimum and maximum values of the region boundary perpendicular to the given one."""
         if side in ["west", "east"]:
-            coord_name = "latitude"
+            coord_name = UM_LATLON[0]
             _min, _max = self.bounds.south, self.bounds.north
         elif side in ["south", "north"]:
-            coord_name = "longitude"
+            coord_name = UM_LATLON[1]
             _min, _max = self.bounds.west, self.bounds.east
         else:
             raise BoundaryError(f"Boundary name '{side}' is not valid")
@@ -104,26 +106,33 @@ class Region(object):
         return sep.join([fmt_lonlat(i["value"], i["coord"]) for i in self])
 
     @classmethod
-    def from_cube(cls, cube, margin=None, margin_units="points"):
+    def from_cube(cls, cube, name=None, margin=None, margin_units="points", shift_lons=False):
         """
         Create a Region from limits of longitude and latitude of the cube.
 
         Parameters
         ----------
         cube: iris.cube.Cube
-            Source cube
+            Source cube.
+        name: str, optional
+            Name for the region. If not given, created automatically from `cube`'s name.
         margin: scalar, optional
-            Use `margin` number of points or degrees to create a region smaller than the cube
+            Use `margin` number of points or degrees to create a region smaller than the cube.
         margin_units: str, optional
             Units of margin. Can be "points" or "degrees".
+        shift_lons: bool, optional
+            Shift longitudes to -180...180.
 
         Returns
         -------
         aeolus.region.Region
         """
-        name = f"extent_of_{cube.name()}"
-        lons = cube.coord("longitude").points
-        lats = cube.coord("latitude").points
+        if name is None:
+            name = f"extent_of_{cube.name()}"
+        lons = cube.coord(UM_LATLON[1]).points
+        if shift_lons:
+            lons = wrap_lons(lons, -180, 360)
+        lats = cube.coord(UM_LATLON[0]).points
         idx0, idx1 = 0, -1
         if margin is not None:
             if margin_units == "points":
