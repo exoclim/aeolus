461ed0080595062b65884c6730bd221297ccddb5
Travis CI <travis@nobody.org>
2019-12-31 11:40:07 +0000

    fix index.html redirection

--- dev/html/html/_static/documentation_options.js
+++ dev/html/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.4.1+28.g7fdbc52',
+    VERSION: '0.4.1+29.g97c06fa',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
