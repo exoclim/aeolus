555767d7a20eca819305d2d3e17498a44c251252
Dennis Sergeev <dennis.sergeev@gmail.com>
2020-06-08 18:04:47 +0100

    ENH: add a dataclass for model-specific names, create UM-dataclass

---
+++ aeolus/model/__init__.py
@@ -0,0 +1,4 @@
+"""Model-specific dictionaries of variable names and coordinates."""
+from .um import um, um_stash
+
+__all__ = ("um", "um_stash")
---
+++ aeolus/model/base.py
@@ -0,0 +1,68 @@
+"""Model-specific dictionaries of variable names and coordinates."""
+from dataclasses import dataclass
+
+
+__all__ = "Model"
+
+
+@dataclass
+class Model:
+    """Base class for model-specific names."""
+
+    # Coordinates
+    t: str = None  # time
+    fcst_ref: str = None  # forecast reference
+    fcst_prd: str = None  # forecast period
+    z: str = None  # height
+    l: str = None  # level number
+    s: str = None  # sigma
+    y: str = None  # latitude
+    x: str = None  # longitude
+    # Variables
+    # Main
+    u: str = None
+    v: str = None
+    w: str = None
+    pres: str = None
+    thta: str = None
+    exner: str = None
+    sh: str = None
+    t_sfc: str = None
+    p_sfc: str = None
+    # Radiation
+    toa_isr: str = None
+    toa_olr: str = None
+    toa_olr_cs: str = None
+    toa_osr: str = None
+    toa_osr_cs: str = None
+    sfc_dn_lw: str = None
+    sfc_dn_lw_cs: str = None
+    sfc_dn_sw: str = None
+    sfc_dn_sw_cs: str = None
+    sfc_net_down_lw: str = None
+    sfc_net_down_sw: str = None
+    lw_up: str = None
+    sw_up: str = None
+    # BL
+    sfc_shf: str = None
+    sfc_lhf: str = None
+    # Extra physics
+    temp: str = None
+    rh: str = None
+    # Precip & Cloud
+    cld_ice_mf: str = None
+    cld_liq_mf: str = None
+    rain_mf: str = None
+    cld_ice_v: str = None
+    cld_liq_v: str = None
+    cld_v: str = None
+    caf: str = None
+    caf_h: str = None
+    caf_m: str = None
+    caf_l: str = None
+    caf_vl: str = None
+    ppn: str = None
+    ls_rain: str = None
+    ls_snow: str = None
+    cv_rain: str = None
+    cv_snow: str = None
