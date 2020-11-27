52f2dde5f9b41d3fb2d3a33514bda682c1b33a98
Dennis Sergeev <dennis.sergeev@gmail.com>
2020-11-27 10:53:12 +0000

    BUG: fix a typo in interp_to_cube_time()

--- src/aeolus/coord.py
+++ src/aeolus/coord.py
@@ -584,6 +584,37 @@ def interp_all_to_pres_lev(cubelist, levels, interpolator=None, model=um):
     return cl_out
 
 
+def interp_to_cube_time(cube_src, cube_tgt, model=um):
+    """
+    Linearly interpolate `cube_src` to `cube_tgt` along the time dimension (`model.t`).
+
+    Forecast period is copied from `cube_tgt`.
+
+    Parameters
+    ----------
+    cube_src: iris.cube.Cube
+        Cube to interpolate.
+    cube_tgt: iris.cube.Cube
+        Cube with time dimension to interpolate to.
+    model: aeolus.model.Model, optional
+        Model class with relevant variable names.
+
+    Returns
+    -------
+    iris.cube.Cube
+        Cube with the time dimension equal to `cube_tgt`.
+    """
+    target = [(model.t, cube_tgt.coord(model.t).points)]
+    out = cube_src.interpolate(target, iris.analysis.Linear())
+    # Replace time coordinates, because interpolation removes bounds.
+    for coord in ["t", "fcst_prd", "fcst_ref"]:
+        try:
+            out.replace_coord(cube_tgt.coord(getattr(model, coord)))
+        except (AttributeError, CoNotFound):
+            pass
+    return out
+
+
 def interp_to_pres_lev(cubelist, constraint, levels, interpolator=None, model=um):
     """
     Interpolate a cube to pressure level(s) using stratify relevel() function.
@@ -652,37 +683,6 @@ def interp_to_single_pres_lev(
     out = interp_to_pres_lev(
         cubelist, constraint, [p_tgt.data], interpolator=interpolator, model=model
     )
-    return out
-
-
-def interp_to_cube_time(cube_src, cube_tgt, model=um):
-    """
-    Linearly interpolate `cube_src` to `cube_tgt` along the time dimension (`model.t`).
-
-    Forecast period is copied from `cube_tgt`.
-
-    Parameters
-    ----------
-    cube_src: iris.cube.Cube
-        Cube to interpolate.
-    cube_tgt: iris.cube.Cube
-        Cube with time dimension to interpolate to.
-    model: aeolus.model.Model, optional
-        Model class with relevant variable names.
-
-    Returns
-    -------
-    iris.cube.Cube
-        Cube with the time dimension equal to `cube_tgt`.
-    """
-    target = [(model.t, cube_tgt.coord(model.t).points)]
-    out = cube_src.interpolate(target, iris.analysis.Linear())
-    # Replace time coordinates, because interpolation removes bounds.
-    for coord in ["t", "fct_prd", "fcst_ref"]:
-        try:
-            out.replace_coord(cube_tgt.coord(getattr(model, coord)))
-        except (AttributeError, CoNotFound):
-            pass
     return out
 
 
