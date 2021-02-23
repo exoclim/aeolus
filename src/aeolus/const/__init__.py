20e006c65ee24e1651b5fe7afccb07a430898ca3
Dennis Sergeev <dennis.sergeev@gmail.com>
2021-02-23 09:33:28 +0000

    Fix an import error

--- src/aeolus/const/__init__.py
+++ src/aeolus/const/__init__.py
@@ -1,5 +1,5 @@
 """Physical constants as collections of scalar `iris` cubes."""
-from .const import get_planet_radius, init_const
+from .const import add_planet_conf_to_cubes, get_planet_radius, init_const
 
 
-__all__ = ("get_planet_radius", "init_const")
+__all__ = ("add_planet_conf_to_cubes", "get_planet_radius", "init_const")
