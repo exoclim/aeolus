baca2fe6c7b1054766261e669e18ff7fd2cc051e
GitHub Action <action@github.com>
2022-01-18 14:02:57 +0000

    Update documentation

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.4.13+15.g13a2c8b',
+    VERSION: '0.4.13+17.gcccf3fa',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
