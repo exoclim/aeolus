cb3af6efa2391aaa5c614dfbd3b2dd72bc08a0ab
Denis Sergeev <dennis.sergeev@gmail.com>
2023-01-05 11:04:02 +0000

    remove deprecated class

--- src/aeolus/core.py
+++ src/aeolus/core.py
@@ -2,7 +2,6 @@
 """Core submodule of aeolus package."""
 from cached_property import cached_property
 
-from iris.cube import CubeList
 from iris.coord_systems import GeogCS
 from iris.exceptions import ConstraintMismatchError as ConMisErr
 
@@ -11,17 +10,12 @@ from .const import add_planet_conf_to_cubes, init_const
 from .coord import CoordContainer
 from .decor import ReprAtmoSimBase
 from .exceptions import _warn
-from .io import load_data, save_cubelist
 from .meta import copy_doc
 from .model import um
 from .region import Region
 from .subset import DimConstr
 
-__all__ = (
-    "AtmoSim",
-    "AtmoSimBase",
-    "Run",
-)
+__all__ = ("AtmoSim", "AtmoSimBase")
 
 
 class AtmoSimBase:
@@ -196,7 +190,7 @@ class AtmoSimBase:
             self._coord_system = GeogCS(semi_major_axis=self.const.radius.data)
         except AttributeError:
             self._coord_system = None
-            _warn("Run initialised without a coordinate system.")
+            _warn("Initialised without a coordinate system.")
 
     def _add_planet_conf_to_cubes(self):
         """Add or update planetary constants container to cube attributes."""
@@ -259,166 +253,3 @@ class AtmoSim(AtmoSimBase):
     @copy_doc(diag.sfc_water_balance)
     def sfc_water_balance(self):
         return diag.sfc_water_balance(self._cubes, const=self.const, model=self.model)
-
-
-class Run:
-    """
-    A single model 'run', i.e. simulation.
-
-    Attributes
-    ----------
-    name: str
-        The run's name.
-    const: aeolus.const.ConstContainer
-        Physical constants used in calculations for this run.
-    model: aeolus.model.Model, optional
-        Model class with relevant coordinate names.
-    """
-
-    attr_keys = ["name", "planet", "model_type", "timestep"]
-
-    def __init__(
-        self,
-        files=None,
-        name="",
-        planet="",
-        const_dir=None,
-        model=um,
-        model_type=None,
-        timestep=None,
-        processed=False,
-    ):
-        """
-        Instantiate a `Run` object.
-
-        Parameters
-        ----------
-        files: str or pathlib.Path, optional
-            Wildcard for loading files.
-        name: str, optional
-            The run's name.
-        planet: str, optional
-            Planet configuration. This is used to get appropriate physical constants.
-            If not given, Earth physical constants are initialised.
-        const_dir: pathlib.Path, optional
-            Path to a folder with JSON files containing constants for a specific planet.
-        model: aeolus.model.Model, optional
-            Model class with relevant coordinate and variable names.
-        model_type: str, optional
-            Type of the model run, global or LAM.
-        timestep: int, optional
-            Model time step in s.
-        parent: aeolus.core.Run, optional
-            Pointer to this run's driving model if this is a LAM-type simulation.
-        children: list, optional
-            List of `aeolus.core.Run` objects if this is a driving model.
-        processed: bool, optional
-            If True, data from `files` is assigned to `proc` attribute.
-
-        See also
-        --------
-        aeolus.const.init_const
-        """
-        _warn(
-            "Run is deprecated and will be removed in the next release. "
-            "Use iris.cube.CubeList instead."
-        )
-        self.name = name
-
-        # Planetary constants
-        self._update_planet(planet=planet, const_dir=const_dir)
-
-        # Model-specific names of variables and coordinates
-        self.model = model
-        self.dim_constr = DimConstr(model=self.model)
-
-        # If the model is global or LAM (nested) and what its driving model is
-        self.model_type = model_type
-        self.timestep = timestep
-        self.processed = processed
-
-        if files:
-            self.load_data(files)
-            try:
-                if self.processed:
-                    cube_yx = self.proc.extract(self.dim_constr.relax.yx)[0]
-                else:
-                    cube_yx = self.raw.extract(self.dim_constr.relax.yx)[0]
-                self.domain = Region.from_cube(cube_yx, name=f"{name}_domain", shift_lons=True)
-            except IndexError:
-                _warn("Run initialised without a domain.")
-        else:
-            _warn("Run initialised without input files")
-
-    def load_data(self, files):
-        """Load cubes."""
-        if self.processed:
-            self.proc = load_data(files)
-            self._add_planet_conf_to_cubes()
-        else:
-            self.raw = load_data(files)
-
-    def _update_planet(self, planet="", const_dir=None):
-        """Add or update planetary constants."""
-        self.planet = planet
-        self.const = init_const(planet, directory=const_dir)
-        try:
-            self.const.radius.convert_units("m")
-            self._coord_system = GeogCS(semi_major_axis=self.const.radius.data)
-        except AttributeError:
-            self._coord_system = None
-            _warn("Run initialised without a coordinate system.")
-
-    def _add_planet_conf_to_cubes(self):
-        """Add or update planetary constants container to cube attributes."""
-        add_planet_conf_to_cubes(self._cubes, self.const)
-
-    def proc_data(self, func=None, **func_args):
-        """
-        Post-process data for easier analysis and store it in `self.proc` attribute.
-
-        Parameters
-        ----------
-        func: callable
-            Function that takes `iris.cube.CubeList` as its first argument.
-        **func_args: dict-like, optional
-            Keyword arguments passed to `func`.
-        """
-        if self.processed:
-            _warn("Run's data is already processed. Skipping.")
-        else:
-            self.proc = CubeList()
-            if callable(func):
-                self.proc = func(self.raw, **func_args)
-            self._add_planet_conf_to_cubes()
-            self.processed = True
-
-    def add_data(self, func=None, **func_args):
-        """
-        Calculate additional diagnostics (of type `iris.cube.Cube`) and add them to `self.proc`.
-
-        Parameters
-        ----------
-        func: callable
-            Function that takes `iris.cube.CubeList` (`self.proc`) as its first argument
-            and appends new cubes to it (and does not return anything).
-        **func_args: dict-like, optional
-            Keyword arguments passed to `func`.
-        """
-        if callable(func):
-            func(self.proc, **func_args)
-
-    def to_file(self, path):
-        """
-        Save `proc` cubelist to a file with appropriate metadata.
-
-        Parameters
-        ----------
-        path: str or pathlib.Path
-            File path.
-        """
-        run_attrs = {}
-        for key in self.attr_keys:
-            if getattr(self, key):
-                run_attrs[key] = str(getattr(self, key))
-        save_cubelist(self.proc, path, **run_attrs)
